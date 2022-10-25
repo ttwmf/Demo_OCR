b592f9319574fd6bd729f7cba53d8394d198c968
ttwmf <thai.hoang@dgvdigital.com>
2022-10-25 21:45:04 +0700

    initial

---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+using System.Linq;
+using computer_vision_quickstart;
+
+namespace ComputerVisionQuickstart
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string subscriptionKey = Secret.KEY;
+            string endpoint = Secret.ENDPOINT;
+            Console.WriteLine("Azure Cognitive Services Computer Vision - .NET quickstart example");
+            Console.WriteLine();
+
+
+            ComputerVisionClient client = Authenticate(endpoint, subscriptionKey);
+            int x = 0;
+            do
+            {
+                Console.WriteLine("Nhập URL:");
+                string URL = Console.ReadLine();
+                // Extract text (OCR) from a URL image using the Read API
+                ReadFileUrl(client, URL).Wait();
+                Console.WriteLine("Continue (Y - 1/ N - 0):");
+                x = Int32.Parse(Console.ReadLine());
+                Console.Clear();
+            } while(x == 1);
+
+        }
+
+        public static ComputerVisionClient Authenticate(string endpoint, string key)
+        {
+            ComputerVisionClient client =
+              new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
+              { Endpoint = endpoint };
+            return client;
+        }
+
+        public static async Task ReadFileUrl(ComputerVisionClient client, string urlFile)
+        {
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("READ FILE FROM URL");
+            Console.WriteLine();
+
+            // Read text from URL
+            var textHeaders = await client.ReadAsync(urlFile);
+            // After the request, get the operation location (operation ID)
+            string operationLocation = textHeaders.OperationLocation;
+            Thread.Sleep(2000);
+
+            // Retrieve the URI where the extracted text will be stored from the Operation-Location header.
+            // We only need the ID and not the full URL
+            const int numberOfCharsInOperationId = 36;
+            string operationId = operationLocation.Substring(operationLocation.Length - numberOfCharsInOperationId);
+
+            // Extract the text
+            ReadOperationResult results;
+            Console.WriteLine($"Extracting text from URL file {Path.GetFileName(urlFile)}...");
+            Console.WriteLine();
+            do
+            {
+                results = await client.GetReadResultAsync(Guid.Parse(operationId));
+            }
+            while ((results.Status == OperationStatusCodes.Running ||
+                results.Status == OperationStatusCodes.NotStarted));
+
+            // Display the found text.
+            Console.WriteLine();
+            var textUrlFileResults = results.AnalyzeResult.ReadResults;
+            foreach (ReadResult page in textUrlFileResults)
+            {
+                foreach (Line line in page.Lines)
+                {
+                    Console.WriteLine(line.Text);
+                }
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ Secret.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace computer_vision_quickstart
+{
+    public static class Secret
+    {
+        // Add your Computer Vision subscription key and endpoint
+        public static string KEY = "Paste your key here";
+        public static string ENDPOINT = "paste your end point here";
+    }
+}
